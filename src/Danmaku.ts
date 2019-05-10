4d26b705bb8b14118fe70e573373e9ded2fb2fb9
zhaoyu <yu.zhao@wm-motor.com>
2019-05-10 11:55:40 +0800

    init

---
+++ src/Danmaku.ts
@@ -0,0 +1,4 @@
+// 弹幕库实例
+export default class Danmaku {
+
+}
---
+++ src/danmaku-plus.ts
@@ -0,0 +1,8 @@
+import Danmaku from "./Danmaku";
+
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+  // import "core-js/fn/array.find"
+  // ...
+
+const mDanmuku:Danmaku =new Danmaku()
+console.log(mDanmuku)
